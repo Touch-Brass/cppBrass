a5d828958125d54eaac3bbc702fa495a051c02cd
philippesh <sarouphim.philippe@gmail.com>
2024-03-06 21:33:51 -0500

    Get serial input

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,16 +1,12 @@
-// #include "MyWidget.h"
-
-// int main(int argc, char **argv)
-// {
-//     QApplication app(argc, argv);
-
-//     MyWidget window;
-//     window.show();
-
-//     return app.exec();
-// }
-
+#include <cctype>
+#include <chrono>
 #include <iostream>
+#include <thread>
+
+#include <stdio.h>
+
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <QApplication>
 #include <QSlider>
@@ -20,6 +16,60 @@
 #include <QLabel>
 
 #include "../src/MainComponent.h"
+
+#define SAMPLES_PER_FFT 1000
+
+
+void read_serial(QSlider* pressureSlider, QSlider* frequencySlider, QSlider* positionSlider){
+    std::string port = "/dev/ttyUSB0";
+    int device = open(port.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
+    std::cout << device << std::endl;
+
+    int16_t* readings = (int16_t*) malloc(sizeof(int16_t) * SAMPLES_PER_FFT);
+
+    while(true){
+        for(int i = 0; i < 1000; i++){
+
+            std::string number = "";
+
+            while(true){
+                char next_char;
+                int n = read(device, &next_char, sizeof(next_char));
+
+                if(n == 0){
+                    std::cout << "Sample Error" << std::endl;
+                    continue;
+                }
+
+                // if(next_char == '\r' || next_char == '\n'){
+                //     break;
+                // }
+
+                if(isdigit(next_char) || next_char == '.'){
+                    number += next_char;
+                } else {
+                    break;
+                }
+            }
+
+            if(number.empty()){
+                i--;
+                continue;
+            }
+
+            readings[i] = atoi(number.c_str());
+            std::cout << readings[i] << std::endl;
+        }
+        
+        // Perform FFT
+
+        QMetaObject::invokeMethod(pressureSlider, "setValue", Qt::QueuedConnection, Q_ARG(int, 50));
+        QMetaObject::invokeMethod(frequencySlider, "setValue", Qt::QueuedConnection, Q_ARG(int, 450));
+        QMetaObject::invokeMethod(positionSlider, "setValue", Qt::QueuedConnection, Q_ARG(int, 300));
+    }
+
+    close(device);
+}
 
 int main(int argc, char **argv)
 {
@@ -48,11 +98,11 @@ int main(int argc, char **argv)
         mc->lipFreqVal = value;
     });
 
-    QLabel label3("Slider Position");
+    QLabel label3("Slide Position");
     QSlider slider3(Qt::Horizontal);
     slider3.setRange(250, 350);
     QObject::connect(&slider3, &QSlider::valueChanged, [&label3, mc](int value) {
-        label3.setText(QString("Slider Position: %1").arg(value));
+        label3.setText(QString("Slide Position: %1").arg(value));
         mc->LVal = (float) value / 100;
     });
 
@@ -64,6 +114,9 @@ int main(int argc, char **argv)
     layout.addWidget(&slider3);
 
     window.show();
+
+    std::thread read_serial_thread(read_serial, &slider1, &slider2, &slider3);
+    read_serial_thread.detach();
 
 
     return app.exec();
